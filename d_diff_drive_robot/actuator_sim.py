ec40885f937071bd52145fbe99453016fc29d036
GMU Student <student@ubuntu.(none)>
2014-09-13 13:01:31 -0400

    fixed actuator issue chaned from buff[3] to s_ins

--- d_diff_drive_robot/actuator_sim.py
+++ d_diff_drive_robot/actuator_sim.py
@@ -6,10 +6,10 @@ def serial_sim(r,ref,buff):
       for i in range(2,(n-1)):
         checkSum = (buff[i] & 0xff) + checkSum
       if ( (buff[n-1] & 0xff) == (~checkSum & 0xff)):
-        if( buff[3] == 0x20 ):     
-          s_id = buff[2] & 0xff
-          s_len = buff[3] & 0xff
-          s_ins = buff[4] & 0xff
+        s_id = buff[2] & 0xff
+        s_len = buff[3] & 0xff
+        s_ins = buff[4] & 0xff
+        if( s_ins == 0x20 ): 
           s_p0 = buff[5] & 0xff
           s_p1 = buff[6] & 0xff
           val = s_p0 + ((s_p1 & 0x03) << 8)
