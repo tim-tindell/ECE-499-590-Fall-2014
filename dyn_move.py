f80e0adb6db8ed108e14d488734637b60fda9ce3
GMU Student <student@ubuntu.(none)>
2014-11-18 10:36:17 -0500

    added better comments to the code

--- dyn_move.py
+++ dyn_move.py
@@ -1,33 +1,3 @@
-#!/usr/bin/env python
-# /* -*-  indent-tabs-mode:t; tab-width: 8; c-basic-offset: 8  -*- */
-# /*
-# Copyright (c) 2014, Daniel M. Lofaro
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the author nor the names of its contributors may
-#       be used to endorse or promote products derived from this software
-#       without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# Based on: https://github.com/thedancomplex/pydynamixel
-# */
 
 import os
 import dynamixel
@@ -85,8 +55,7 @@ def main(settings):
         actuator.torque_limit = 800
         actuator.max_torque = 800
     
-    # Randomly vary servo position within a small range
-
+    #vary servo position within a small range
     enc = 0.0;
     angleToMove = -0.1
     cmdY = 0
@@ -100,27 +69,23 @@ def main(settings):
         cmdY+= (angY) * angleToMove
         # move camera center right
         cmdX+= (angX) * (angleToMove)
-   ############# Get the current feed-forward (state) 
+   #####send goal positions to the actuators
         for actuator in myActuators:
             if ( actuator.id == 2):
                 actuator.goal_position = rad2dyn(cmdY)
             elif (actuator.id == 3):
                 actuator.goal_position = rad2dyn(cmdX)
         net.synchronize()
-        
+        ###Smooth out the movements
         for i in range(100):
             angY += ang.Y
             angX += ang.X
         angY/100
         angX/100
 
-        for actuator in myActuators:
-            actuator.read_all()
-            time.sleep(0.01)
-            if ( actuator.id == 2):
-                enc = dyn2rad(actuator.current_position)
-#	print encoder.enc[ha.NKY], " : ", encoder.enc[ha.NK1], " : ", encoder.enc[ha.NK2]
-        print 'cmdY = ', cmdY, '  enc = ', enc
+		time.sleep(0.02)
+        print 'cmdY = ', cmdY
+		print 'cmdX= ', cmdX
 
 
 def validateInput(userInput, rangeMin, rangeMax):
